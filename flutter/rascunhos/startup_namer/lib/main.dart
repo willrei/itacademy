16b3608ad48153f958dbe47dbe4f6d6ae9075f6e
Willian Reichert <wnreichert@inf.ufrgs.br>
2021-11-06 21:57:15 -0300

    Primeira parte do codelab Startup Namer

---
+++ flutter/rascunhos/startup_namer/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  RandomWordsState createState() {
+    return RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final suggestions = <WordPair>[];
+  final biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: buildSuggestions(),
+    );
+  }
+
+  Widget buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= suggestions.length) {
+          suggestions.addAll(generateWordPairs().take(10));
+        }
+        return buildRow(suggestions[index]);
+      },
+    );
+  }
+
+  Widget buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: biggerFont,
+      ),
+    );
+  }
+}
